647c543cf03577bd0898663b2bed6d32cc32156d
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-24 18:19:25 +0200

    implement various math functions

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 /**
- * Contains all the functions exported from the WebAssembly module instance.
+ * @description Wrapper which contains all functions exported from WebAssembly module instance.
  * @type {{
- *  sumIntNums: (a: number, b: number) => number
+ *  sumIntNums: (a: number, b: number) => number;
+ *  subIntNums: (a: number, b: number) => number;
+ *  mulIntNums: (a: number, b: number) => number;
+ *  divIntNums: (a: number, b: number) => number;
  * }}
  */
 const exports = {};
 
 async function loadWasm() {
   const source = await fetch('./main.wasm');
-  const webAssemblyObject = await WebAssembly.instantiateStreaming(source);
+  const webAssemblyObject = await WebAssembly.instantiateStreaming(source, {});
 
   exports.sumIntNums = webAssemblyObject.instance.exports['sum_int_nums'];
 
   if (!exports.sumIntNums) {
     throw new Error('"sumIntNums" export not found');
+  }
+
+  exports.subIntNums = webAssemblyObject.instance.exports['sub_int_nums'];
+
+  if (!exports.subIntNums) {
+    throw new Error('"subIntNums" export not found');
+  }
+
+  exports.mulIntNums = webAssemblyObject.instance.exports['mul_int_nums'];
+
+  if (!exports.mulIntNums) {
+    throw new Error('"mulIntNums" export not found');
+  }
+
+  exports.divIntNums = webAssemblyObject.instance.exports['div_int_nums'];
+
+  if (!exports.divIntNums) {
+    throw new Error('"divIntNums" export not found');
   }
 }
 
--- main.c
+++ main.c
@@ -5,3 +5,21 @@ int sum_int_nums(int a, int b)
 {
     return a + b;
 }
+
+EMSCRIPTEN_KEEPALIVE
+int sub_int_nums(int a, int b)
+{
+    return a - b;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int mul_int_nums(int a, int b)
+{
+    return a * b;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int div_int_nums(int a, int b)
+{
+    return a / b;
+}
